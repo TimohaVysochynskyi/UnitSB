f998ce791c1278b8079146d396aff8449fc30136
TimohaVysochynskyi <70945682+TimohaVysochynskyi@users.noreply.github.com>
2025-01-09 21:17:56 +0200

    feat: header hero services projects

---
+++ scripts/projects-swiper.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".projects__swiper-wrapper", {
+  slidesPerView: "auto",
+  spaceBetween: 40,
+  scrollbar: {
+    el: ".projects__swiper-scrollbar",
+    hide: false,
+    draggable: true,
+  },
+});
